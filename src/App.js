206c6e8b3698db573870cafa3ca2871068500d57
Karthick <pkarthicbz@live.in>
2023-04-29 10:25:39 +0530

    add images for carousal

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Home from "./components/home";
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+      <Home />
     </div>
   );
 }
---
+++ src/components/home.js
@@ -0,0 +1,11 @@
+import image from '../images/carousal_images/camera1.jpg'
+
+function Home() {
+  return (
+    <div className="home">
+      <img src={image} alt="camera"/>
+    </div>
+  );
+}
+
+export default Home;
