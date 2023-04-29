2dce1c7a7fd3d0e41e1f5fe74285fa0ea9254d68
Karthick <pkarthicbz@live.in>
2023-04-29 12:21:44 +0530

    add autoimage slider

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,36 @@
-import image from '../images/carousal_images/camera1.jpg'
+import { useState, useEffect } from 'react';
+// import image1 from '../images/carousal_images/camera1.jpg'
+// import image2 from '../images/carousal_images/camera2.jpg'
+
+// import ImageSlider from 'image-slider-react';
 
 function Home() {
-  return (
-    <div className="home">
-      <img src={image} alt="camera"/>
-    </div>
-  );
+    const [index, setIndex] = useState(0);
+    const [images, setImages] = useState([]);
+
+    function importImages(links){
+        return links.keys().map(links);
+    }
+
+    useEffect(()=>{
+        const timer = setInterval(()=>{
+            if(index > 3){
+                setIndex(0);
+            }else{
+                setIndex((prevIndex)=>prevIndex+1);
+            }
+        }, 3000)
+        const carousalImages = importImages(require.context('../images/carousal_images/'), false, '.jpg');
+        setImages(carousalImages);
+
+        return ()=>clearInterval(timer);
+    }, [index])
+
+    return (
+        <div className="home">
+            <img className="image-carousel" src={images[index]} alt='camera'/>
+        </div>
+    );
 }
 
 export default Home;
