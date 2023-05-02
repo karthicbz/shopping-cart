e51ca624a243323504dda973d0515912370414ad
karthicbz <pkarthicbz@live.in>
2023-05-02 12:10:08 +0530

    count quantity when same item is added to cart

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Navigation from "./components/navigation";
 import Shop from "./components/shop";
 import Cart from "./components/cart";
 import SideTag from "./components/sideTag";
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import ProductData from './datas/data';
 
 export const CartCountContext = createContext(0);
@@ -14,17 +14,50 @@ function App() {
   const [cartItems, setCartItems] = useState([]);
 
   function handleClick(e){
-    // setValue((prevValue)=>prevValue+1);
-    // console.log(document.querySelector('.products'));
+
     const product = ProductData.data.filter(product=>{
       if(product.product_name === e.target.dataset.prod){
         return product;
       }
     });
 
-    setCartItems(
-      [...cartItems, {product_name:product[0].product_name, product_image:product[0].product_image, quantity:1, product_price: product[0].product_price}]
+    if(cartItems.length === 0){
+      setCartItems(
+        [...cartItems, {product_name:product[0].product_name, product_image:product[0].product_image, quantity:1, product_price: product[0].product_price}]
       );
+    }else{
+      // console.log(isItemFound(e));
+      const isFound = isItemFound(e);
+      if(isFound.found === false){
+        setCartItems(
+          [...cartItems, {product_name:product[0].product_name, product_image:product[0].product_image, quantity:1, product_price: product[0].product_price}]
+        );
+      }else{
+          cartItems.map(item=>{
+            if(item.product_name === e.target.dataset.prod){
+              item.quantity += 1;
+            }
+          })
+      }
+    }
+
+  }
+
+  useEffect(()=>{
+    console.log(cartItems);
+  }, [cartItems]);
+
+  const isItemFound = (e)=>{
+    const status = {found:false, index:0};
+    cartItems.forEach((item, i=0)=>{
+      if(item.product_name === e.target.dataset.prod){
+        status.found = true;
+        status.index = i;
+      }else{
+        i += 1;
+      }
+    })
+    return status;
   }
 
   return (
