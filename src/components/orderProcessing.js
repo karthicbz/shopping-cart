d64c5ccd007cc0c8166c04d727dfa8220f490071
karthicbz <pkarthicbz@live.in>
2023-05-03 14:31:45 +0530

    add order processing page

--- src/components/orderProcessing.js
+++ src/components/orderProcessing.js
@@ -1,9 +1,17 @@
+import { useWindowSize } from "@react-hook/window-size"
+import Confetti from "react-confetti"
+import { Link } from "react-router-dom"
+
 const OrderProcessing = ()=>{
+    const {width, height} = useWindowSize()
     return(
-        <div>
+        <div className="order-processing-section">
+            <Confetti width={width} height={height}/>
+        <div className="order-completed-message">
             <p>Thank you for your order</p>
             <p>Your order will be processed soon</p>
-            <button>Home</button>
+            <Link to={"/"}><button className="order-to-home">Home</button></Link>
+        </div>
         </div>
     )
 }
